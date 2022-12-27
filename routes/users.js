7e4796034631f89626a1deeef6d079d425cb712e
arifpatanduk <arifpatanduk2@gmail.com>
2022-12-28 01:46:12 +0700

    model init

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-const express = require("express");
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
+const UsersModel = require("../models/users")
 
 // main users endpoint
-router.get("/", (req, res) => {
-  res.json("users");
-});
+router.get("/", async (req, res) => {
+  const users = await UsersModel.findAll()
+  console.log(users);
 
-module.exports = router;
+  res.status(200).json({
+    data: "test",
+    metadata: "test1234",
+  })
+})
+
+module.exports = router
